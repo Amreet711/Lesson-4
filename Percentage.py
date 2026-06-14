3639c085da475eb0d85057a0d5899990270475d1
Amreet711 <sunny1935@gmail.com>
2026-06-14 18:04:59 +1000

    Percentage

---
+++ Percentage.py
@@ -0,0 +1,5 @@
+Maths=int(input("What score did you get for maths?:"))
+English=int(input("What score did you get for english?:"))
+Science=int(input("What score did you get for science?:"))
+Percentage=((Maths+English+Science)/300)*100
+print(Percentage)
